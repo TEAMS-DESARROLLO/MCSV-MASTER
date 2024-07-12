55b240beed5d486e928c1002dd5f66b908eedb1c
gfachuy <gfachuy@indracompany.com>
2024-07-12 10:26:24 -0500

    Adding layer controller to Region , Rol and UnitMeasure

--- src/main/java/com/bussinesdomain/maestros/MaestrosApplication.java
+++ src/main/java/com/bussinesdomain/maestros/MaestrosApplication.java
@@ -2,11 +2,10 @@ package com.bussinesdomain.maestros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MaestrosApplication {
 
 	public static void main(String[] args) {
