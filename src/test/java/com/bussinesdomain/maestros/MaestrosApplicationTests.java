b37e31583ca14ca5abd84975ee62284a2ba58c68
Carlos Rojas <carojasc@indracompany.com>
2025-01-02 14:26:20 -0500

    Modificacion 2

--- src/test/java/com/bussinesdomain/maestros/MaestrosApplicationTests.java
+++ src/test/java/com/bussinesdomain/maestros/MaestrosApplicationTests.java
@@ -3,11 +3,11 @@ package com.bussinesdomain.maestros;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MaestrosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
